036bd79d051add711ac34a2cdc0c5c4fb53df5cc
ClintFix <clint.fix@gmail.com>
2021-04-07 14:02:51 -0600

    index and server setup

--- api/server.js
+++ api/server.js
@@ -1,10 +1,15 @@
 const express = require('express');
+//import users-router
+const usersRouter = require('./users/users-model')
 
 const server = express();
 
 // remember express by default cannot parse JSON in request bodies
-
+server.use(express.json());
 // global middlewares and the user's router need to be connected here
+
+// Connect userRouter:
+server.use('/api/users', usersRouter)
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // require your server and launch it
+const server = require('express')
+
+server.listen(4000, () => {
+    console.log('Server running on port 4000. http://localhost:4000')
+})
