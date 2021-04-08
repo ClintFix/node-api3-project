f12405e54cc6fab4d4c777d39b3361f8d1600ea4
ClintFix <clint.fix@gmail.com>
2021-04-08 14:16:31 -0600

    lets try again

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 const server = require('./api/server');
 const dotenv = require('dotenv').config();
-const cors = require("cors");
 const port = process.env.PORT || 9000;
-
-server.use(cors());
 
 server.listen(port, () => {
     console.log(`Server is alive on port ${port}`)
