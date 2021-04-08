d10311ef82cf8060cdd04579707fd1ac5294c2bd
ClintFix <clint.fix@gmail.com>
2021-04-08 13:53:05 -0600

    setup env file

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
-// require your server and launch it
-const server = require('./api/server')
+const server = require('./api/server');
+const dotenv = require('dotenv').config();
+const cors = require("cors");
+const port = process.env.PORT || 9000;
 
-server.listen(4000, () => {
-    console.log('Server running on port 4000. http://localhost:4000')
+server.use(cors());
+
+server.listen(port, () => {
+    console.log(`Server is alive on port ${port}`)
 })
